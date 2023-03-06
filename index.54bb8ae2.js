fa874533d3e4ba76bbfa6599391f89668e3f161c
dumenko111 <101402719+dumenko111@users.noreply.github.com>
2023-03-06 14:15:48 +0000

    Deploying to gh-pages from @ dumenko111/hw_countries@6546f263a43c28a67b21a29c5dd3da248c2ad1a2 🚀

--- index.54bb8ae2.js
+++
@@ -1,2 +0,0 @@
-!function(){var o,e;document.querySelector(".js-search-form");(o="ukraine",e="https://restcountries.com/v2/name/".concat(o),fetch(e).then((function(o){return o.ok?o.json():[]})).catch((function(o){o({text:"Network error!"}),console.log(o)}))).then(console.log)}();
-//# sourceMappingURL=index.54bb8ae2.js.map
--- index.db098be6.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".js-search-form");var e;(e="ukraine",fetch(`https://restcountries.com/v2/name/${e}`).then((e=>e.ok?e.json():[])).catch((e=>{e({text:"Network error!"}),console.log(e)}))).then(console.log);
-//# sourceMappingURL=index.db098be6.js.map
